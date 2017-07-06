dc936c80425696fa24387390877c1ea4626a6a72
zhongxinzhi <1123858432@qq.com>
2017-07-06 19:25:35 +0800

    初始化

--- src/components/categoryBar.vue
+++ src/components/categoryBar.vue
@@ -1,20 +1,40 @@
 <template>
+  <div class="wrapper" ref="wrapper">
+    <div class="bar" v-for="item in data" :style="{ background: item.color }">
 
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
+  .wrapper{
+    width: 100%;
+    overflow: hidden;
+  }
+  .bar{
+    float: left;
+  }
 </style>
 <script>
 	export default {
-		props: [],
 		data() {
 			return {}
 		},
-		methods: {},
+		props: {
+			data: [Array],
+      minWidth: [Number]
+    },
+		methods: {
+
+    },
 		created() {
 
 		},
 		mounted() {
+			var vm = this;
+			var el = vm.$el;
+			console.log(el)
+			console.log()
 		}
 	}
 </script>
--- src/modules/index.vue
+++ src/modules/index.vue
@@ -1,25 +1,52 @@
 <template>
   <div class="index-wrapper">
-    <select-link :selects="selects" :data="selectData"></select-link>
+    <div class="selects-wrapper">
+      <select-link :selects="selects" :data="selectData"></select-link>
+    </div>
+    <div class="cbar-wrapper">
+      <category-bar :data="barData"></category-bar>
+    </div>
   </div>
+
 </template>
 
 <style lang="less" scoped>
   .index-wrapper{
     text-align: left;
   }
+  .cbar-wrapper{
+    width: 500px;
+  }
 </style>
 <script>
     import SelectLink from 'components/selectLink'
+    import CategoryBar from 'components/categoryBar'
     import regions from 'lib/china_regions/region_dumps.json'
 
     export default {
         components: {
-          SelectLink
+          SelectLink,
+          CategoryBar
         },
         data() {
             return {
             	selects: ['province', 'city', 'country'],
+              barOption: {
+            		minWidth: 200,
+                Height: 60
+              },
+              barData:[
+                {
+                	label: '未处理',
+                  value: 500,
+                  color: '#000'
+                },
+                {
+                  label: '已处理',
+                  value: 500,
+                  color: '#f00'
+                }
+              ],
             }
         },
         methods: {},
@@ -27,7 +54,6 @@
 
         },
         mounted() {
-        	console.log($)
         },
         computed: {
         	//value label children格式
