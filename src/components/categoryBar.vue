d0b0a0e5c4a4b3bc9170edf6a69b2ac94a947e83
zhongxinzhi <1123858432@qq.com>
2017-07-06 22:32:00 +0800

    categoryBar

--- src/components/categoryBar.vue
+++ src/components/categoryBar.vue
@@ -1,40 +1,71 @@
 <template>
   <div class="wrapper" ref="wrapper">
-    <div class="bar" v-for="item in data" :style="{ background: item.color }">
+    <div class="bar"
+         v-for="item in data"
+         :style="{ background: item.color, height: option.height + 'px', width: item.width + 'px'}">
 
     </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-  .wrapper{
+  .wrapper {
     width: 100%;
-    overflow: hidden;
   }
-  .bar{
+
+  .bar {
     float: left;
+    position: relative;
+  }
+
+  .bar-children {
+    position: absolute;
+    height: 100%;
+    left: 0;
+    right: 0;
+    top: 100%;
+
   }
 </style>
 <script>
-	export default {
-		data() {
-			return {}
-		},
-		props: {
-			data: [Array],
-      minWidth: [Number]
+  export default {
+    data() {
+      return {}
     },
-		methods: {
+    props: {
+      data: [Array],
+      option: [Object]
+    },
+    methods: {
+    	setWidths() {
+        var _this = this;
+        var parentWidth = $('.wrapper').parent().width();
+        var values = this.data.map((item) => {
+          return item.value
+        })
+        var count = values.reduce((pre,cur) => {return pre + cur})
+        var minWidth = this.option.minWidth;
+        var widths = values.map((value)=>{
+        	return parentWidth * value / count
+        })
+        widths.forEach((width)=>{
+        	_this.data.forEach((item)=>{
+        		item.width = width
+            console.log(width)
+          })
+        })
+
+      }
+    },
+    created() {
+
+
 
     },
-		created() {
+    mounted() {
+      var vm = this;
+      vm.setWidths.call(vm)
 
-		},
-		mounted() {
-			var vm = this;
-			var el = vm.$el;
-			console.log(el)
-			console.log()
-		}
-	}
+    }
+  }
 </script>
--- src/modules/index.vue
+++ src/modules/index.vue
@@ -4,65 +4,81 @@
       <select-link :selects="selects" :data="selectData"></select-link>
     </div>
     <div class="cbar-wrapper">
-      <category-bar :data="barData"></category-bar>
+      <category-bar :data="barData" :option="barOptions"></category-bar>
     </div>
   </div>
 
 </template>
 
 <style lang="less" scoped>
-  .index-wrapper{
+  .index-wrapper {
     text-align: left;
   }
-  .cbar-wrapper{
-    width: 500px;
+
+  .cbar-wrapper {
+    width: 1000px;
   }
 </style>
 <script>
-    import SelectLink from 'components/selectLink'
-    import CategoryBar from 'components/categoryBar'
-    import regions from 'lib/china_regions/region_dumps.json'
+  import SelectLink from 'components/selectLink'
+  import CategoryBar from 'components/categoryBar'
+  import regions from 'lib/china_regions/region_dumps.json'
 
-    export default {
-        components: {
-          SelectLink,
-          CategoryBar
+  export default {
+    components: {
+      SelectLink,
+      CategoryBar
+    },
+    data() {
+      return {
+        selects: ['province', 'city', 'country'],
+        barOptions: {
+          minWidth: 200,
+          height: 60,
+          childMinWidth: 30
         },
-        data() {
-            return {
-            	selects: ['province', 'city', 'country'],
-              barOption: {
-            		minWidth: 200,
-                Height: 60
+        barData: [
+          {
+            name: '未处理',
+            value: 500,
+            color: '#000',
+            width: 0,
+            children: [
+              {
+                name: '未响应',
+                value: 200,
+                color: '#bbb'
               },
-              barData:[
-                {
-                	label: '未处理',
-                  value: 500,
-                  color: '#000'
-                },
-                {
-                  label: '已处理',
-                  value: 500,
-                  color: '#f00'
-                }
-              ],
-            }
-        },
-        methods: {},
-        created() {
-
-        },
-        mounted() {
-        },
-        computed: {
-        	//value label children格式
-          selectData: function(){
-          	var data = regions.slice(),ret=[];
-          	data.forEach((item) => {
-
-            })
+              {
+                name: '未下发',
+                value: 300,
+                width: 0,
+                color: '#777'
+              }
+            ]
+          },
+          {
+            name: '已处理',
+            value: 500,
+            color: '#f00'
           }
-        }
+        ],
+      }
+    },
+    methods: {},
+    created() {
+
+    },
+    mounted() {
+    },
+    computed: {
+      //value label children格式
+      selectData: function () {
+        var data = regions.slice(), ret = [];
+        data.forEach((item) => {
+
+        })
+      }
     }
+  }
 </script>
